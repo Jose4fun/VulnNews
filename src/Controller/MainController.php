18c2f56e1443ac2d4aa6ac785935e53e7d21c1c8
chengyushi <chengyushi98@gmail.com>
2020-01-30 16:37:41 +0800

    Small changes

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,11 +8,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/main", name="main")
+     * @Route("/", name="main")
      */
     public function index()
     {
         return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    public function news()
+    {
+        /**
+         * @Route("/news", name="news")
+         */
+        return $this->render('main/news.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
